fd3bf1d221525620ae8779f7488ade9b8f86da7d
kumatan <kumatan@kumymelody.jp>
2021-08-23 07:01:56 +0900

    TAG119 2021/08/23   Compiler Driver   　トレース向け情報にOPMが抜けていたので追加

--- mucomDotNETCommon/Common.cs
+++ mucomDotNETCommon/Common.cs
@@ -104,9 +104,9 @@ namespace mucomDotNET.Common
             switch (ChipIndex)
             {
                 case 0:
-                case 1:
-                    return 0;
                 case 2:
+                    return 0;
+                case 1:
                 case 3:
                     return 1;
                 case 4:
