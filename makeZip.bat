072810d7437dbd449e868a86026aadac3797062c
kumatan <kumatan@kumymelody.jp>
2021-09-22 08:05:33 +0900

    TAG132 2021/09/22   .NET5向けプロジェクト追加(Playerを除く)

--- makeZip.bat
+++ makeZip.bat
@@ -7,6 +7,11 @@ mkdir output\toWav
 mkdir output\toVgm
 mkdir output\PCMTool
 mkdir output\Samples
+mkdir output\NET5
+mkdir output\NET5\compiler
+mkdir output\NET5\toWav
+mkdir output\NET5\toVgm
+mkdir output\NET5\PCMTool
 
 del /Q .\output\*.*
 del /Q .\output\compiler\*.*
@@ -14,12 +19,23 @@ del /Q .\output\player\*.*
 del /Q .\output\toWav\*.*
 del /Q .\output\toVgm\*.*
 del /Q .\output\Samples\*.*
+del /Q .\output\NET5\*.*
+del /Q .\output\NET5\compiler\*.*
+del /Q .\output\NET5\toWav\*.*
+del /Q .\output\NET5\toVgm\*.*
+
 xcopy .\mucomDotNETConsole.Net4\bin\Release\net472\*.* .\output\compiler\ /E /R /Y /I /K
 xcopy .\mucomDotNETPlayer\bin\Release\net472\*.* .\output\player\ /E /R /Y /I /K
 xcopy .\Wav.Net4\bin\Release\net472\*.* .\output\toWav\ /E /R /Y /I /K
 xcopy .\Vgm.Net4\bin\Release\net472\*.* .\output\toVgm\ /E /R /Y /I /K
 xcopy .\PCMTool.Net4\bin\Release\net472\*.* .\output\PCMTool\ /E /R /Y /I /K
 xcopy .\Samples\*.* .\output\Samples\ /E /R /Y /I /K
+
+xcopy .\Compiler_NET5\bin\Release\net5.0\*.* .\output\NET5\compiler\ /E /R /Y /I /K
+xcopy .\Wav_NET5\bin\Release\net5.0\*.* .\output\NET5\toWav\ /E /R /Y /I /K
+xcopy .\Vgm_NET5\bin\Release\net5.0\*.* .\output\NET5\toVgm\ /E /R /Y /I /K
+xcopy .\PCMTool_NET5\bin\Release\net5.0\*.* .\output\NET5\PCMTool\ /E /R /Y /I /K
+
 del /Q .\output\*.pdb
 del /Q .\output\*.config
 del /Q .\output\compiler\*.pdb
@@ -31,6 +47,14 @@ del /Q .\output\toWav\*.config
 del /Q .\output\toVgm\*.pdb
 del /Q .\output\toVgm\*.config
 del /Q .\output\bin.zip
+
+del /Q .\output\NET5\compiler\*.pdb
+del /Q .\output\NET5\compiler\*.config
+del /Q .\output\NET5\toWav\*.pdb
+del /Q .\output\NET5\toWav\*.config
+del /Q .\output\NET5\toVgm\*.pdb
+del /Q .\output\NET5\toVgm\*.config
+
 copy .\CHANGE.txt .\output\
 copy .\README.md .\output\
 copy .\usage.txt .\output\
